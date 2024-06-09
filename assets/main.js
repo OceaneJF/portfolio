86e8f8beb6d7aec9570dec5e41596d542bc8f6b9
Nicolas Castex <nicolas.castex.pro@gmail.com>
2024-06-09 17:01:12 +0200

    fix menu navigation mobile et carte titre

--- assets/main.js
+++ assets/main.js
@@ -1,27 +1,34 @@
+const about = document.querySelector('a[href="#qui"]');
+const menuTel = document.querySelector(".menu-tel");
 
+about.addEventListener("click", () => {
+  menuTel.checked = false;
+  console.log(menuTel);
+  console.log(menuTel.checked);
+});
 
-const threshold = .1
+const threshold = 0.1;
 const options = {
-    root: null,
-    rootMargin: '0px',
-    threshold
-}
+  root: null,
+  rootMargin: "0px",
+  threshold,
+};
 
 const handleIntersect = function (entries, observer) {
-    entries.forEach(function (entry) {
-        if (entry.intersectionRatio > threshold) {
-            entry.target.classList.remove('reveal')
-            observer.unobserve(entry.target)
-        }
-    })
-}
+  entries.forEach(function (entry) {
+    if (entry.intersectionRatio > threshold) {
+      entry.target.classList.remove("reveal");
+      observer.unobserve(entry.target);
+    }
+  });
+};
 
-document.documentElement.classList.add('reveal-loaded')
+document.documentElement.classList.add("reveal-loaded");
 
 window.addEventListener("DOMContentLoaded", function () {
-    const observer = new IntersectionObserver(handleIntersect, options)
-    const targets = document.querySelectorAll('.reveal')
-    targets.forEach(function (target) {
-        observer.observe(target)
-    })
-})
+  const observer = new IntersectionObserver(handleIntersect, options);
+  const targets = document.querySelectorAll(".reveal");
+  targets.forEach(function (target) {
+    observer.observe(target);
+  });
+});
